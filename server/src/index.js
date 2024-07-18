a4048f52f842e275666dc57121fdcc119c4586a2
Ivan Krug <139096035+IvanKrug@users.noreply.github.com>
2024-07-17 20:06:42 -0400

    test deploy

--- server/src/index.js
+++ server/src/index.js
@@ -2,5 +2,5 @@ import app from './app.js';
 import 'dotenv/config'
 import { connectDB } from './db.js';
 connectDB();
-app.listen(process.env.PORT);
-console.log('server en puerto ', process.env.PORT);
+app.listen(process.env.PORT || 3000);
+console.log('server en puerto ', process.env.PORT || 3000);
